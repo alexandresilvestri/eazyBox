4551f81015e15f5d6195c2f0b168ff5ecf29e50a
Alexandre Silvestri <airtontunni@gmail.com>
2026-03-23 21:08:40 -0300

    remove unnecessary comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,20 @@
 class SessionsController < ApplicationController
   before_action :set_session, only: %i[ show edit update destroy ]
 
-  # GET /sessions or /sessions.json
   def index
     @sessions = Session.all
   end
 
-  # GET /sessions/1 or /sessions/1.json
   def show
   end
 
-  # GET /sessions/new
   def new
     @session = Session.new
   end
 
-  # GET /sessions/1/edit
   def edit
   end
 
-  # POST /sessions or /sessions.json
   def create
     @session = Session.new(session_params)
 
@@ -34,7 +29,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /sessions/1 or /sessions/1.json
   def update
     respond_to do |format|
       if @session.update(session_params)
@@ -47,7 +41,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  # DELETE /sessions/1 or /sessions/1.json
   def destroy
     @session.destroy!
 
@@ -58,12 +51,10 @@ class SessionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_session
       @session = Session.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def session_params
       params.expect(session: [ :start_time, :max_capacity, :name ])
     end
--- app/controllers/weekly_schedules_controller.rb
+++ app/controllers/weekly_schedules_controller.rb
@@ -1,25 +1,20 @@
 class WeeklySchedulesController < ApplicationController
   before_action :set_weekly_schedule, only: %i[ show edit update destroy ]
 
-  # GET /weekly_schedules or /weekly_schedules.json
   def index
     @weekly_schedules = WeeklySchedule.all
   end
 
-  # GET /weekly_schedules/1 or /weekly_schedules/1.json
   def show
   end
 
-  # GET /weekly_schedules/new
   def new
     @weekly_schedule = WeeklySchedule.new
   end
 
-  # GET /weekly_schedules/1/edit
   def edit
   end
 
-  # POST /weekly_schedules or /weekly_schedules.json
   def create
     @weekly_schedule = WeeklySchedule.new(weekly_schedule_params)
 
@@ -34,7 +29,6 @@ class WeeklySchedulesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /weekly_schedules/1 or /weekly_schedules/1.json
   def update
     respond_to do |format|
       if @weekly_schedule.update(weekly_schedule_params)
@@ -47,7 +41,6 @@ class WeeklySchedulesController < ApplicationController
     end
   end
 
-  # DELETE /weekly_schedules/1 or /weekly_schedules/1.json
   def destroy
     @weekly_schedule.destroy!
 
@@ -58,12 +51,10 @@ class WeeklySchedulesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_weekly_schedule
       @weekly_schedule = WeeklySchedule.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def weekly_schedule_params
       params.expect(weekly_schedule: [ :day_of_week, :start_time, :max_capacity ])
     end

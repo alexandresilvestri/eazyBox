1b8c8783cff7f4a91bc3368f5b5e8b8a45e14dff
Alexandre Silvestri <airtontunni@gmail.com>
2026-03-29 08:13:08 -0300

    enable uuid extension on pg

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_28_192205) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_29_000813) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "gyms", force: :cascade do |t|
+  create_table "gyms", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "corporate_name"
     t.datetime "created_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.datetime "updated_at", null: false
   end
 end
